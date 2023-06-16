893170d4f5958d90b842dde6136684bd29bfe66e
niccoloruberti <nikiruberti@gmail.com>
2023-06-16 17:29:23 +0200

    esercizio base

--- js/script.js
+++ js/script.js
@@ -5,8 +5,24 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            randomEmails: []
         }
-    }
+    },
+    mounted() {
+        this.generateTenRandomEmails();
+    },
+    methods: {
+        // funzione che genara 10 emails random
+        generateTenRandomEmails() {
+            for (let i = 1; i <= 10; i++) {
+                // utilizzo la librearia axios per eseguire la librearia dell'API
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                                        // inserisco la mail creata all'interno dell'array
+                                        console.log(result.data.response)
+                                        this.randomEmails.push(result.data.response);
+                })
+                };
+            }
+        },
 }).mount('#app');
 
